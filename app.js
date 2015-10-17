83f0cff797288bfc4088d1a0fe324956db6a246d
adsaroje <adsaroje@hotmail.com>
2015-10-17 16:15:28 -0500

    Primer intento de trabajar con socket.io
    Solo agregué el ejemplo basico de socket.io

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+//configurar socket.io
+var io = require('socket.io')(app);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +27,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+//manejar conexiones socket.io
+io.on('connection', function(socket){
+    socket.emit('news', {hello: 'world'});
+    socket.on('my other event', function(data){
+        console.log(data);
+    });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
